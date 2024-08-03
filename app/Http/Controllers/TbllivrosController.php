0b4420a9dd4fbabf8ffd6f67a4ae96d72188cf47
Haniel2607 <haniel.schramm@etec.sp.gov.br>
2024-08-02 22:52:35 -0300

    funções novas

---
+++ app/Http/Controllers/TbllivrosController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TbllivrosController extends Controller
+{
+    //construir o crud.
+    
+    public function index(){
+
+    }
+
+    public function show(){
+    
+    }
+
+    public function store(){
+    
+    }
+
+    public function update(){
+    
+    }
+
+    public function destroy(){
+    
+    }
+}
